d859601a70dde31da86a9534d9db6e770fc2c461
xtian <benedictramirez78@gmail.com>
2023-06-08 23:01:26 +0800

    recreate mina webpage in next.js

---
+++ app/page.js
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+      <>
+        <header>
+          Hello
+        </header>
+        <aside>
+          Aside
+        </aside>
+        <main>
+          Main
+        </main>
+        <footer>
+          footer
+        </footer>
+      </>
+  )
+}
