bbe1c4d6d4e385e3529b4399ef9e3911f75725c0
xtian <benedictramirez78@gmail.com>
2023-06-08 23:28:11 +0800

    changed font from inter to fira sans

--- app/page.js
+++ app/page.js
@@ -4,16 +4,13 @@ export default function Home() {
   return (
       <>
         <header>
-          <div className='text-6xl'>Hello this is a header</div>
+          <div className='text-6xl'>Mina Webpage</div>
         </header>
-        <aside>
-          Aside
-        </aside>
         <main>
-          Main
+          Main section
         </main>
         <footer>
-          footer
+          this is a footer
         </footer>
       </>
   )
