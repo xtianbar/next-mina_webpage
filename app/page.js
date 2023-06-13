da4e71d003583ca78383f954e8bc0c83e02fc2f7
xtian <benedictramirez78@gmail.com>
2023-06-13 23:37:20 +0800

    replace $ to html entity and add alt to images

--- app/page.js
+++ app/page.js
@@ -34,24 +34,24 @@ export default function Home() {
           </div>
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 justify-between mb-10'>
             <div className='text-center'>
-              <Image src={candy} />
+              <Image alt='candy flowerpot' src={candy} />
               <p className='text-2xl pt-4 pb-2'>'Candy' Flowerpot</p>
-              <p className='pb-3 text-xl'><span className='text-[#828282] line-through'>$59.90</span> $49.90</p>
+              <p className='pb-3 text-xl'><span className='text-[#828282] line-through'>&#36;59.90</span> &#36;49.90</p>
             </div>
             <div className='text-center'>
-              <Image src={drop}/>
+              <Image alt='drop vase' src={drop}/>
               <p className='text-2xl pt-4 pb-2'>'Drop' Vase</p>
-              <p className='pb-3 text-xl'><span className='text-[#828282] line-through'>$49.90</span> $40.90</p>
+              <p className='pb-3 text-xl'><span className='text-[#828282] line-through'>&#36;49.90</span> &#36;40.90</p>
             </div>
             <div className='text-center'>
-              <Image src={wave} />
+              <Image alt='wave vase' src={wave} />
               <p className='text-2xl pt-4 pb-2'>'Wave' Vase</p>
               <p className='pb-3 text-xl uppercase text-[#828282]'>Sold out</p>
             </div>
             <div  className='text-center'>
-              <Image src={sun} />
+              <Image alt='sun vase' src={sun} />
               <p className='text-2xl pt-4 pb-2'>'Sun' Vase</p>
-              <p className='pb-3 text-xl'>$69.90</p>
+              <p className='pb-3 text-xl'>&#36;69.90</p>
             </div>
           </div>
         </div>
@@ -73,7 +73,7 @@ export default function Home() {
           
           <div className='grid grid-cols-1 md:grid-cols-2 gap-3 justify-between mt-6 mb-10'>
             <div className='text-start'>
-              <Image src={aboutMina} />
+              <Image alt='about mina' src={aboutMina} />
               <p className='text-3xl font-medium pt-8 pb-2 leading-10'>About Mina</p>
               <p className='pb-3 text-xl text-[#828282] leading-10'>Mina is a brand born out of a passion for ceramics.</p>
               <div className='mb-1 mt-2'>
@@ -81,7 +81,7 @@ export default function Home() {
               </div>
             </div>
             <div  className='text-start mt-16 md:mt-0'>
-              <Image src={sustain} />
+              <Image alt='sustainability' src={sustain} />
               <p className='text-3xl font-medium pt-8 pb-2 leading-10'>Sustainability</p>
               <p className='pb-3 text-xl text-[#828282] leading-10'>Mina is an eco-friendly brand that cares about sustainability.</p>
               <div className='mb-1 mt-2'>
@@ -111,17 +111,17 @@ export default function Home() {
           </div>
           <div className='flex gap-3 justify-between mb-10'>
             <div>
-              <Image src={ig1} />
+              <Image alt='ig1' src={ig1} />
             </div>
             <div>
-              <Image src={ig2}/>
+              <Image alt='ig2' src={ig2}/>
             </div>
             <div>
-              <Image src={ig3} />
+              <Image alt='ig3' src={ig3} />
             </div>
           </div>
           <div className='mb-10'>
-            <button type='button' className='text-white bg-[#4d205a] px-10 py-4 rounded-lg'>@mina_studio</button>
+            <button type='button' className='text-white bg-[#4d205a] px-10 py-4 rounded-lg'>&#64;mina_studio</button>
           </div>
         </div>
       </>
